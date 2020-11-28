ba010edde08e66438428d5782c09d51d8ef6cf3c
Alison Sipos <alison.sipos@icloud.com>
2020-11-27 19:52:31 -0700

    Initial Commit.

---
+++ client/src/app.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
