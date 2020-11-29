4026236ae5956e27f4aa108131d8c0d40318f788
Alison Sipos <alison.sipos@icloud.com>
2020-11-29 16:27:08 -0700

    Added styles class to intro paragraph.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -14,9 +14,8 @@ const App = () => {
       <Container>
         <Jumbotron fluid>
           <Container>
-            <h1>Beans Affirmations</h1>
-            <p>We talk to ourselves through our thoughts and inner monologue more than anyone else talkes to us. It is no wonder our thoughts play a large role in our overall success and happiness. It is so important to find ways to improve our mindset and speak kindly to ourselves, least risk falling into negative thought patterns.</p>
-            <p>Affirmations are a powerful way to improve your mindset on a daily basis, and research has shown they can increase our feelings of self-worth.</p>
+            <img src="https://beans-affirmations.s3.us-east-2.amazonaws.com/Beans_Long.png" alt="beans logo" className="header-logo"/>
+            <p className="beans-intro">We talk to ourselves through our thoughts and inner monologue more than anyone else talkes to us. It is no wonder our thoughts play a large role in our overall success and happiness. It is so important to find ways to improve our mindset and speak kindly to ourselves, least risk falling into negative thought patterns. Research has shown affirmations can increase our feelings of self-worth, that&apos;s where beans come in! Beans are a powerful way to improve your mindset on a daily basis.</p>
           </Container>
         </Jumbotron>
       </Container>
