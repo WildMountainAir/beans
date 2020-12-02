0fedf4085572390d7d7372f4b7ecb1f3eba6a5b0
Alison Sipos <alison.sipos@icloud.com>
2020-12-02 14:54:53 -0700

    Corrected export syntax.

--- client/src/lib/routes.js
+++ client/src/lib/routes.js
@@ -1,9 +1,11 @@
 const Path = 'http://localhost:3008';
 
-export const listBeans = (callback) => {
-  const path = `${Path}/beans/`;
+const listBeans = (callback) => {
+  const path = `${Path}/beans`;
   fetch(path)
     .then((result) => result.json())
     .then((data) => { callback(null, data); })
     .catch((error) => { callback(error, null); });
 };
+
+export default listBeans;
