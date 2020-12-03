520107f65bd7e5191e74b274192b75e26e29babb
Alison Sipos <alison.sipos@icloud.com>
2020-12-02 17:02:18 -0700

    Organized beans into a table, added style classes.

--- client/src/components/beansList.jsx
+++ client/src/components/beansList.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
+// import Table from 'react-bootstrap/Table';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
-const BeansList = ({ beans }) => {
+const BeansList = ({ beans}) => {
 
   return (
     <div>
-      {beans.map((bean) => {
-        return (
-          <div key={uuidv4()}>
-            {bean.category}
-            {bean.bean}
-          </div>
-        )
-      })}
+      <table className="beans-table">
+        <thead>
+          <tr>
+            <th className="beans-table-header">Category</th>
+            <th className="beans-table-header">Bean Affirmations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            // eslint-disable-next-line react/prop-types
+            beans.map((bean) => {
+            return (
+              <tr key={uuidv4()}>
+                <td className="beans-table-data beans-category">{bean.category}</td>
+                <td className="beans-table-data beans-bean">{bean.bean}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
