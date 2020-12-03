f25fb7c85c176c819a4176179fbafe8eb675364b
Alison Sipos <alison.sipos@icloud.com>
2020-12-02 17:04:15 -0700

    Added Category Buttons not functional, event handlers created, and added a View All Button that conditionally renders all beans.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -9,6 +9,11 @@ import BeansList from './components/beansList.jsx';
 
 const App = () => {
   const [beans, setAllBeans] = useState([]);
+  const [viewBeans, setViewBeans] = useState(false);
+  const [viewSelfLove, setViewSelfLove] = useState(false);
+  const [viewConfidence, setViewConfidence] = useState(false);
+  const [viewAcceptance, setViewAcceptance] = useState(false);
+  const [viewGrowth, setViewGrowth] = useState(false);
 
   useEffect(() => {
     listBeans((error, response) => {
@@ -16,17 +21,55 @@ const App = () => {
     })
   }, []);
 
+  const handleView = () => {
+    setViewBeans(!viewBeans);
+  }
+
+  const handleSelfLove = () => {
+    setViewSelfLove(!viewSelfLove);
+  }
+
+  const handleConfidence = () => {
+    setViewConfidence(!viewConfidence);
+  }
+
+  const handleAcceptance = () => {
+    setViewAcceptance(!viewAcceptance);
+  }
+
+  const handleGrowth = () => {
+    setViewGrowth(!viewGrowth);
+  }
+
   return (
     <div>
       <Container>
         <Jumbotron fluid>
           <Container>
             <img src="https://beans-affirmations.s3.us-east-2.amazonaws.com/Beans_Long.png" alt="beans logo" className="header-logo"/>
-            <p className="beans-intro">We talk to ourselves through our thoughts and inner monologue more than anyone else talkes to us. It is no wonder our thoughts play a large role in our overall success and happiness. It is so important to find ways to improve our mindset and speak kindly to ourselves, least risk falling into negative thought patterns. Research has shown affirmations can increase our feelings of self-worth, that&apos;s where beans come in! Beans are a powerful way to improve your mindset on a daily basis.</p>
+            <p className="beans-intro">We talk to ourselves through our thoughts and inner monologue more than anyone else talks to us. It is no wonder our thoughts play a large role in our overall success and happiness. It is so important to find ways to improve our mindset and speak kindly to ourselves, or risk falling into negative thought patterns. That&apos;s where beans come in! Beans are a powerful way to improve your mindset on a daily basis.</p>
           </Container>
         </Jumbotron>
-        {/* Affirmations */}
-        <BeansList beans={beans} />
+        <div className="category-box">
+        <button className="button-root category" onClick={handleSelfLove}>Self Love</button>
+        <button className="button-root category" onClick={handleConfidence}>Confidence</button>
+        <button className="button-root category" onClick={handleAcceptance}>Acceptance</button>
+        <button className="button-root category" onClick={handleGrowth}>Growth</button>
+        </div>
+        { 
+          viewBeans 
+          ? 
+          <div>
+            <button className="button-root hide-all" onClick={handleView}> 
+              Hide All Beans
+            </button>
+            <BeansList beans={beans} />
+          </div> 
+          : 
+          <button className="button-root view-all" onClick={handleView}> 
+            View All Beans
+          </button>
+        }
       </Container>
     </div>
   );
