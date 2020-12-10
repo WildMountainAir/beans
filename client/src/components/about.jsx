e4b46cd7daf2920eb5bb39af27cf04107ead346b
Alison Sipos <alison.sipos@icloud.com>
2020-12-09 22:47:11 -0700

    Made new component describing the app.

---
+++ client/src/components/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <p className="beans-intro"> Beans offers on demand affirmations when you need them most. We talk to ourselves through our thoughts and inner monologue more than anyone else talks to us. It is so important to find ways to improve our mindset and speak kindly to ourselves, or risk falling into negative thought patterns. Our thoughts and how we talk to ourselves plays a large role in our overall success and happiness. That&apos;s where Beans comes in! Beans are affirmations and are a powerful way to improve your mindset on a daily basis.</p>
+    </div>
+  )
+}
+
+export default About;
