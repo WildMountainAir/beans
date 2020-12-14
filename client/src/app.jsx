630af4c3bfa007eb54db8a3fa32d935b45e1d9bb
Alison Sipos <alison.sipos@icloud.com>
2020-12-14 15:15:13 -0700

    Updated credentials.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -30,6 +30,7 @@ const App = () => {
 
   useEffect(() => {
     listBeans((error, response) => {
+      console.log('list response', response);
       error ? 'could not get beans' : setAllBeans(response);
     })
     // getSelfLove((error, response) => {
