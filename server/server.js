d22ac13f7f9b9f9b67a037db2d77a8aff9e3f545
Alison Sipos <alison.sipos@icloud.com>
2020-12-14 12:12:07 -0700

    Updated Port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 // const PORT = 3008;
-const PORT = process.env.PORT || 3008;
+// const PORT = process.env.PORT || 3008;
+const PORT = 3306;
 const queries = require('../database/queries');
 
 app.use(express.static('../client/dist'));
