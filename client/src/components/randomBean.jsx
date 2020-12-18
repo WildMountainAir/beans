222ea54463fab050f8fb86691bdbb728d00c9315
Alison Sipos <alison.sipos@icloud.com>
2020-12-18 16:35:16 -0700

    added undefined conditional

--- client/src/components/randomBean.jsx
+++ client/src/components/randomBean.jsx
@@ -8,7 +8,7 @@ const RandomBean = ({beans}) => {
   return (
     <div>
       <table className="beans-table">
-      <thead className="beans-table-data beans-title">Focus your intention on {beans[randomNum].category}</thead>
+      <thead className="beans-table-data beans-title">Focus your intention on {beans[randomNum].category !== undefined ? beans[randomNum].category : beans[5].category }</thead>
         <tbody>
               <tr key={uuidv4()}>
                 <td className="beans-table-data beans-bean"><img className="icon" src="https://beans-affirmations.s3.us-east-2.amazonaws.com/Beans_Icon.png" alt="logo icon"></img> {beans[randomNum].bean}</td>
