7c3a191ea79ce5ac25cdc0c4cbd04e802cf85266
Alison Sipos <alison.sipos@icloud.com>
2020-12-21 16:10:48 -0700

    Added new fetch request and path for acceptance beans. BUG: returning all beans.

--- client/src/lib/routes.js
+++ client/src/lib/routes.js
@@ -1,11 +1,19 @@
 // const Path = 'http://localhost:3008'
 
 const listBeans = (callback) => {
-  const path = `/beans`;
+  const path = `http://localhost:3008/beans`;
   fetch(path)
     .then((result) => result.json())
     .then((data) => { callback(null, data); })
     .catch((error) => { callback(error, null); });
 };
 
-export { listBeans as default }
+export const listAcceptanceBeans = (callback) => {
+  const path =`http://localhost:3008/beans/Acceptance`;
+  fetch(path)
+    .then((result) => result.json())
+    .then((data) => { callback(null, data); })
+    .catch((error) => { callback(error, null); });
+};
+
+export default listBeans;
