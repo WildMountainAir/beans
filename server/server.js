921d0fe4ca7cc76c9bd0ce51d0ba61ed17b7186e
Alison Sipos <alison.sipos@icloud.com>
2020-12-21 16:10:04 -0700

    Added get request for acceptance beans.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
-// const PORT = 3008;
-const PORT = process.env.PORT || 3008;
-// const PORT = process.env.PORT;
-// const PORT = 3306 || process.env.PORT;
+const PORT = 3008;
+// const PORT = process.env.PORT || 3008;
 const queries = require('../database/queries');
 
 app.use(express.static('../client/dist'));
 app.use(express.json());
-
-// app.post("/", (req, res) => {
-//     res.send('index');
-// })
 
 app.get("/beans", (req,res) => {
   queries.allBeans((error, data) => {
@@ -19,17 +13,13 @@ app.get("/beans", (req,res) => {
   });
 });
 
-app.get("/beans/:category", (req,res) => {
-    queries.categoryBeans(req.params, (error, data) => {
-      error ? res.status(404).send(error) : res.status(202).send(data);
-    });
+app.get("/beans/Acceptance", (req,res) => {
+  queries.acceptanceBeansQ((error, data) => {
+    // getting expected results
+    error ? res.status(404).send(error) : res.status(202).send(data);
   });
+});
 
-// app.get("/beans/:favorite", (req,res) => {
-//   queries.favoriteBeans(req.params, (error, data) => {
-//     error ? res.status(404).send(error) : res.status(202).send(data);
-//   })
-// })
 // Listening for requests on the PORT
 app.listen(PORT, () => {
     console.log(`🛥  Server is running and listening on port ${PORT} 🛥`)
