716008efb1587df115f3e938b7875700f050c0cb
Alison Sipos <alison.sipos@icloud.com>
2020-12-21 22:13:30 -0700

    Added routes for all categories

--- client/src/lib/routes.js
+++ client/src/lib/routes.js
@@ -16,4 +16,27 @@ export const listAcceptanceBeans = (callback) => {
     .catch((error) => { callback(error, null); });
 };
 
+export const listGrowthBeans = (callback) => {
+  const path =`http://localhost:3008/beans/Growth`;
+  fetch(path)
+    .then((result) => result.json())
+    .then((data) => { callback(null, data); })
+    .catch((error) => { callback(error, null); });
+};
+
+export const listConfidenceBeans = (callback) => {
+  const path =`http://localhost:3008/beans/Confidence`;
+  fetch(path)
+    .then((result) => result.json())
+    .then((data) => { callback(null, data); })
+    .catch((error) => { callback(error, null); });
+};
+
+export const listSelfLoveBeans = (callback) => {
+  const path =`http://localhost:3008/beans/Self-Love`;
+  fetch(path)
+    .then((result) => result.json())
+    .then((data) => { callback(null, data); })
+    .catch((error) => { callback(error, null); });
+};
 export default listBeans;
