62ffc6bd7f1069e3304fa629fbccba032c7c0f4a
Alison Sipos <alison.sipos@icloud.com>
2020-12-22 16:43:04 -0700

    Created new search component.

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      <p className="beans-intro"> Beans offers on demand affirmations when you need them most. We talk to ourselves through our thoughts and inner monologue more than anyone else talks to us. It is so important to find ways to improve our mindset and speak kindly to ourselves, or risk falling into negative thought patterns. Our thoughts and how we talk to ourselves plays a large role in our overall success and happiness. That&apos;s where Beans comes in! Beans are affirmations and are a powerful way to improve your mindset on a daily basis.</p>
+      <p className="beans-intro"> Beans offers on demand affirmations when you need them most. We talk to ourselves through our thoughts and inner monologue more than anyone else talks to us. It is so important to find ways to improve our mindset and speak kindly to ourselves, or risk falling into negative thought patterns. Our thoughts and how we talk to ourselves plays a large role in our overall success and happiness. Beans affirmations are positive statements that can help you to challenge and overcome self-sabotaging and negative thoughts. When you repeat them often, and believe in them, you can start to make positive changes.</p>
     </div>
   )
 }
---
+++ client/src/components/searchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+
+const Search = () => {
+
+  return (
+    <Container>
+      Search Bar Will Go Here
+    </Container>
+  )
+}
+
+export default Search;
