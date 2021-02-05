5c6ffa679911a17eda7cd6f045e5502492ac3f26
Alison Sipos <alison.sipos@icloud.com>
2021-02-05 15:25:20 -0700

    Updated props, mapped over new props, updated prop types.

--- client/src/components/growthBeans.jsx
+++ client/src/components/growthBeans.jsx
@@ -3,22 +3,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
-const GrowthBeans = ({beans}) => {
-  const growthArray = [];
-  // eslint-disable-next-line react/prop-types
-  for (var i = 0; i < beans.length; i ++) {
-    if (beans[i].category === 'Growth') {
-      growthArray.push(beans[i]);
-    }
-  }
+const GrowthBeans = ({growthBeans}) => {
+
   return (
     <div>
       <table className="beans-table">
       <thead className="beans-table-data beans-title">Focus your intention on Growth</thead>
         <tbody>
           {
-            // eslint-disable-next-line react/prop-types
-            growthArray.map((bean) => {
+            growthBeans.map((bean) => {
             return (
               <tr key={uuidv4()}>
                 <td className="beans-table-data beans-bean"><img className="icon" src="https://beans-affirmations.s3.us-east-2.amazonaws.com/Beans_Icon.png" alt="logo icon"></img> {bean.bean}</td>
@@ -32,9 +25,10 @@ const GrowthBeans = ({beans}) => {
 }
 
 GrowthBeans.propTypes = {
-  beans: PropTypes.shape({
+  growthBeans: PropTypes.shape({
     category: PropTypes.string,
     bean: PropTypes.string,
+    map: PropTypes.func,
   }).isRequired
 }
 
