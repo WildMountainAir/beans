b855bb32656712223309be6fb4cf890c7e7dc13f
Alison Sipos <alison.sipos@icloud.com>
2021-02-05 15:24:34 -0700

    Updated props, mapped over new props, updated prop types.

--- client/src/components/acceptanceBeans.jsx
+++ client/src/components/acceptanceBeans.jsx
@@ -3,40 +3,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 
-const AcceptanceBeans = ({acceptanceBeans, beans}) => {
-  console.log('acceptance beans in component', acceptanceBeans)
-  const acceptanceArray = [];
-  // eslint-disable-next-line react/prop-types
-  for (var i = 0; i < beans.length; i ++) {
-    if (beans[i].category === 'Acceptance') {
-      acceptanceArray.push(beans[i]);
-    }
-  }
+const AcceptanceBeans = ({acceptanceBeans}) => {
+  
   return (
     <div>
       <table className="beans-table">
       <thead className="beans-table-data beans-title">Focus your intention on Acceptance</thead>
         <tbody>
           {
-            // eslint-disable-next-line react/prop-types
-            acceptanceArray.map((bean) => {
+            acceptanceBeans.map((bean) => {
             return (
               <tr key={uuidv4()}>
                 <td className="beans-table-data beans-bean"><img className="icon" src="https://beans-affirmations.s3.us-east-2.amazonaws.com/Beans_Icon.png" alt="logo icon"></img> {bean.bean}</td>
               </tr>
             )
             })
-          }
-          <p>ACCEPTANCE BEANS</p>
-          {
-            // eslint-disable-next-line react/prop-types
-            acceptanceBeans.map((bean) => {
-              return (
-                <tr key={uuidv4()}>
-                  <td className="beans-table-data beans-bean"><img className="icon" src="https://beans-affirmations.s3.us-east-2.amazonaws.com/Beans_Icon.png" alt="logo icon"></img> {bean.bean}</td>
-                </tr>
-              )
-              })
           }
         </tbody>
       </table>
@@ -52,7 +33,8 @@ AcceptanceBeans.propTypes = {
   acceptanceBeans: PropTypes.shape({
     category: PropTypes.string,
     bean: PropTypes.string,
-  }).isRequired
+    map: PropTypes.func,
+  }).isRequired,
 }
 
 export default AcceptanceBeans;
